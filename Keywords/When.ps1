2a60b0a79db4849e6830726f53f8c3e85852c834
Robert Kozak <rkozak@gmail.com>
2016-03-08 18:25:32 +0800

    Adding missing files

---
+++ Keywords/When.ps1
@@ -0,0 +1,73 @@
+﻿function When:
+{
+    <#
+        .SYNOPSIS
+        When: command for Cidney Pipelines. Used between Stage: and Do:
+        The When: command lets you specify a an event to listen for that you will run its script block against 
+        
+        .DESCRIPTION
+        When: command for Cidney Pipelines. Used between Stage: and Do:
+        The When: command lets you specify a an event to listen for that you will run its script block against 
+        
+        .EXAMPLE
+        .\HelloWorld.ps1
+
+        Pipeline: HelloWorld {
+            Stage: One {
+                When: 'MyEvent' {
+                    Do: { Ipconfig}
+                }
+            }
+        }
+
+        Run ipconfig when MyEvent is fired.
+
+        .LINK
+        Pipeline:
+        Stage:
+        Do:
+        On:
+        Dsc:
+        At:
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, Position = 0)]
+        [object]
+        $Event,
+        [Parameter(Mandatory, Position = 1)]
+        [scriptblock]
+        $WhenBlock,
+        [object]
+        $EventObject,
+        [switch]
+        $Wait,
+        [Int32]
+        $Timeout = 300
+    )
+
+    $script = $WhenBlock.ToString().Trim() 
+  
+    $script +=  @'
+    
+    $null = $eventSubscriber | Unregister-Event
+    $null = $eventSubscriber.Action | Remove-Job
+'@
+
+    $eventAction = [ScriptBlock]::Create($script)
+    if ($EventObject)
+    {
+        $null = Register-ObjectEvent $object $Event -Action $eventAction -MaxTriggerCount 1 
+    }
+    else
+    {
+        $null = Register-EngineEvent -SourceIdentifier $Event -Action $eventAction -MaxTriggerCount 1 
+    }
+
+    if ($wait)
+    {
+        Wait-Event $Event -Timeout $Timeout
+    }
+}
---
+++ Samples/HelloWorld-Event.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module Cidney -Force
+
+Pipeline: HelloWorld {
+    Stage: One {
+        When: MyEvent {
+            Write-Host 'HelloWorld from Stage One'
+           # Start-Process C:\Windows\System32\notepad.exe
+        } -Wait -Timeout 10
+    }
+     
+    Stage: Two {
+        Write-Output 'Stage Two'
+        $null = New-Event MyEvent
+    }
+} -Verbose -ShowProgress
+
+
