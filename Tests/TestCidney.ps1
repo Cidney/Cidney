1037eb0549f41052d70d99d8d9ab50228afbb467
Robert Kozak <rkozak@gmail.com>
2016-03-22 13:54:49 +0800

    AppVeyor seems to execute PS scripts in a runspace

--- Tests/TestCidney.ps1
+++ Tests/TestCidney.ps1
@@ -1,17 +1,9 @@
-﻿# I dont have credentials to access the appveyor machine this runs on. So instead of 
-# using the On: keyword to import the modules I import them directly in the Do: block
-#
-
-Import-Module 'c:\projects\Cidney\Cidney.psd1' -force -Verbose
-Import-Module Pester -Force -Verbose
-
-pipeline: CidneyBuild {
+﻿pipeline: CidneyBuild {
     $Path = (Get-Module Cidney).ModuleBase
     Stage: Pester {
-        Do: { Invoke-Pester -Script "$path\Tests\Pipeline.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Stage.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Do.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Pipeline.Do.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Performance.Tests.ps1" }
+        Do: { Import-Module 'c:\projects\Cidney\Cidney.psd1', Pester; Invoke-Pester -Script "$path\Tests\Pipeline.Tests.ps1" }
+        Do: { Import-Module 'c:\projects\Cidney\Cidney.psd1', Pester; Invoke-Pester -Script "$path\Tests\Stage.Tests.ps1" }
+        Do: { Import-Module 'c:\projects\Cidney\Cidney.psd1', Pester; Invoke-Pester -Script "$path\Tests\Do.Tests.ps1" }
+        Do: { Import-Module 'c:\projects\Cidney\Cidney.psd1', Pester; Invoke-Pester -Script "$path\Tests\Pipeline.Do.Tests.ps1" }
     }
 } -Invoke -Verbose
