18bbdb88f796d0b487431798146b58732511090e
Robert Kozak <rkozak@gmail.com>
2016-03-27 14:27:52 +0800

    updated test script

--- Tests/TestCidney.ps1
+++ Tests/TestCidney.ps1
@@ -2,12 +2,13 @@
 $path = 'c:\Projects\Cidney\'
 Import-module (Join-path $path 'Cidney.psd1')
 pipeline: CidneyBuild {
+    $base = (Get-Module Cidney).ModuleBase
     Import-Module Pester
     Stage: Pester {
-        Do: { Invoke-Pester -Script "$path\Tests\Pipeline.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Stage.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Do.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\Pipeline.Do.Tests.ps1" }
-        Do: { Invoke-Pester -Script "$path\Tests\When.Tests.ps1" }
+        Do: { Invoke-Pester -Script "$base\Tests\Pipeline.Tests.ps1" }
+        Do: { Invoke-Pester -Script "$base\Tests\Stage.Tests.ps1" }
+        Do: { Invoke-Pester -Script "$base\Tests\Do.Tests.ps1" }
+        Do: { Invoke-Pester -Script "$base\Tests\Pipeline.Do.Tests.ps1" }
+        Do: { Invoke-Pester -Script "$base\Tests\When.Tests.ps1" }
     }
 } -Invoke -Verbose
